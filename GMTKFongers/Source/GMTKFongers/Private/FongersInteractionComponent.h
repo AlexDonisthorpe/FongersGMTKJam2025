233602052dcac0d042f2f0bd4a82abc9ea15d8b6
Alex Donisthorpe <62384267+AlexDonisthorpe@users.noreply.github.com>
2025-08-01 16:38:49 +0100

    Interaction Component & Potion Actor
    WIP

---
+++ GMTKFongers/Source/GMTKFongers/Private/FongersInteractionComponent.h
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "FongersInteractionComponent.generated.h"
+
+class AActor;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent), Blueprintable, BlueprintType)
+class UFongersInteractionComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+	UFongersInteractionComponent();	
+public:
+	UFUNCTION(BlueprintPure)
+	FName GetItemName() const { return ItemName; };
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintPure)
+	bool CanInteract() const;
+
+	UFUNCTION(BlueprintPure)
+	AActor* GetInteractingActor() const { return CurrentInteractingActor.Get(); }
+
+	UFUNCTION(BlueprintCallable)
+	virtual bool RequestTapInteraction(AActor* InteractingActor);
+
+	UFUNCTION(Server, Reliable)
+	virtual void ServerStartTapInteraction(AActor* InteractingActor);
+
+	UFUNCTION(BlueprintNativeEvent)
+	void PerformTapInteraction(AActor* InteractingActor);
+
+	// Make sure you call this when you're done
+	UFUNCTION(BlueprintNativeEvent)
+	void InteractionComplete();
+	
+private:
+	int32 InhibitRequests { 0 };
+
+	UPROPERTY(EditAnywhere)
+	FName ItemName;
+
+	UPROPERTY(Transient)
+	TWeakObjectPtr<AActor> CurrentInteractingActor;
+};
